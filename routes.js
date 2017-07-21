c720e2fdd343c890cf5602d8f61ad6c52abd55c6
anfelo <af.osorio1341@gmail.com>
2017-07-20 20:23:31 -0500

    Added the valid url route

--- routes.js
+++ routes.js
@@ -1,12 +1,38 @@
 'use strict';
 const express = require('express');
+const validUrl = require('valid-url');
 const router = express.Router();
+
+// Validate incomming request :url
+router.param('url', function (req, res, next, url) {
+	if (validUrl.isUri(url)){
+			console.log('Looks like an URI:', url);
+	} else {
+			console.log('Not a URI');
+	}
+	// const validUrl = url.match(/^[http|https]+:\//);
+	// const short = {"original-url": null, "short-url": null};
+	// if(validUrl) {
+	// 	short["original-url"] = validUrl;
+	// 	req.shortUrl = short;
+	// 	return next();
+	// }	
+	// req.shortUrl = 'Invalid Url';
+	return next();
+});
 
 // GET 
 // Route for displaying the api user instructions
 router.get('/', function(req, res, next) {
 	// Render home view
-	res.send('Hello');
+	res.render('index');
+});
+
+// GET /:url
+// Route for displaying the shorten url
+router.get('/api/:url(https?:\/\/[\\da-z\.-]+\.[a-z]{2,6}$)', function(req, res, next) {
+	// Render home view
+	res.json(req.params);
 });
 
 module.exports = router;
